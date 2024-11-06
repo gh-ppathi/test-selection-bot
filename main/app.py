779c9fdbbc4436ee5d89dedb8faa2956162270f4
gh-ppathi <ppathi@guardanthealth.com>
2024-11-05 17:53:27 -0800

    main v2

--- main/app.py
+++ main/app.py
@@ -116,7 +116,7 @@ def main():
                 # Define tabs based on the username
                 if username in ["salesrep1", "salesrep2"]:
                     # Create tabs with an extra "My Territory Performance" tab for sales reps
-                    tabs = st.tabs(["New GH Patient", "My Territory Performance"])
+                    tabs = st.tabs(["New GH Patient", "Sales Enhancer"])
                     tab1, tab3 = tabs[0], tabs[1]
                 else:
                     # Create "New GH Patient" and "Existing GH Patient" tabs for other users
@@ -136,18 +136,18 @@ def main():
                         selected_customer = st.selectbox("Select an existing Patient:", existing_customers)
 
                         if selected_customer == "Patient A":
-                            st.write('Patient Summary:')
+                            st.header('Patient Summary:')
                             st.write('Patient A with CRC cancer started the journey at GH during stage III with a single **Reveal** monitoring test and came back when the cancer progressed to stage IV for **G360**. They then got on the 1st line lung and had no response to therapy thus getting an other **G360** and are currently following up with **Reveal**.')
                             st.image('https://2024-q4-hackathon-team5.s3.us-west-2.amazonaws.com/images/Patient+1.png', caption="Patient A's Patient Journey")
                         elif selected_customer == "Patient B":
-                            st.write('Patient Summary:')
+                            st.header('Patient Summary:')
                             st.write('Patient B came in during late stage with CRC cancer and ordered **G360** and are currently monitoring through **Reveal** test. ')
                             st.image('https://2024-q4-hackathon-team5.s3.us-west-2.amazonaws.com/images/Patient+2.png', use_column_width=True, caption="Patient B's Patient Journey")
 
                 # My Territory Performance Tab (only for sales reps)
                 if username in ["salesrep1", "salesrep2"]:
                     with tab3:
-                        st.write(f'{names[username]}, Here is your **{territory[username]}** Territory Overview')
+                        st.title(f'{names[username]}, Here is your **{territory[username]}** Territory Overview')
                         st.image('https://2024-q4-hackathon-team5.s3.us-west-2.amazonaws.com/images/Field+Rep+Persona+at+NPI.png', caption="My Territory Performance")
 
 if __name__ == "__main__":
